bac7c98d1c590b4987533e8f462e522ce84c0dc2
Aldo Camacho <aldo.camacho98@gmail.com>
2021-12-15 12:38:17 -0800

    Day 2: Dive! - Part Two

--- 2-dive/dive.py
+++ 2-dive/dive.py
@@ -1,26 +1,45 @@
 import sys
 
+
 def load_data(filename):
     data = []
-    with open(filename, 'r') as f:
+    with open(filename, "r") as f:
         for line in f:
-            line = line.strip().split(' ')
+            line = line.strip().split(" ")
             line[1] = int(line[1])
             data.append(line)
 
         return data
 
 
+# Part One
 def get_position(data):
     horizontal = 0
     depth = 0
     for entry in data:
-        if entry[0] == 'forward':
+        if entry[0] == "forward":
             horizontal += entry[1]
-        elif entry[0] == 'down':
+        elif entry[0] == "down":
             depth += entry[1]
-        elif entry[0] == 'up':
+        elif entry[0] == "up":
             depth -= entry[1]
+
+    return [horizontal, depth]
+
+
+# Part Two
+def get_position_aim(data):
+    horizontal = 0
+    depth = 0
+    aim = 0
+    for entry in data:
+        if entry[0] == "forward":
+            horizontal += entry[1]
+            depth += aim * entry[1]
+        elif entry[0] == "down":
+            aim += entry[1]
+        elif entry[0] == "up":
+            aim -= entry[1]
 
     return [horizontal, depth]
 
@@ -33,5 +52,9 @@ if __name__ == "__main__":
         exit()
 
     position = get_position(data)
+    print(position)
+    print(position[0] * position[1])
+
+    position = get_position_aim(data)
     print(position)
     print(position[0] * position[1])
