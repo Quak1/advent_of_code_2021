a3abb74d2b95362e5ef58101990d7d4830e03ee9
Aldo Camacho <aldo.camacho98@gmail.com>
2021-12-15 12:08:19 -0800

    Sonar Sweep - Part Two

--- 1-sonar-sweep/sonar_sweep.py
+++ 1-sonar-sweep/sonar_sweep.py
@@ -1,3 +1,5 @@
+import sys
+
 def load_data(filename):
     with open(filename, 'r') as f:
         data = []
@@ -18,7 +20,25 @@ def count_increments(data):
     return increment_count
 
 
+def generate_sliding(data):
+    sliding_window = []
+
+    for i in range(len(data) - 2):
+        sliding_window.append(data[i] + data[i+1] + data[i+2])
+
+    return sliding_window
+
+
 if __name__ == "__main__":
-    data = load_data('input.txt')
+    try:
+        data = load_data(sys.argv[1])
+    except IndexError:
+        print("Please specify data file as your first argument")
+        exit()
+
     count = count_increments(data)
     print('increment count:', count)
+
+    data_sliding = generate_sliding(data)
+    count_sliding = count_increments(data_sliding)
+    print('increment count sliding:', count_sliding)
